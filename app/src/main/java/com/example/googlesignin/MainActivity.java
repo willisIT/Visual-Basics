7ee24a86e646597b5a794a9002222b097fa46755
willisIT <williamskyei24@gmail.com>
2019-03-10 14:23:49 -0700

    Deleted A Code

--- app/src/main/java/com/example/googlesignin/MainActivity.java
+++ app/src/main/java/com/example/googlesignin/MainActivity.java
@@ -17,8 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseApp.initializeApp(this);
-
         mAuth = FirebaseAuth.getInstance();
     }
 
